462f99ac9849d1ebff7cd9908a218fd7b8f69c4f
menezesja <santmjad@gmail.com>
2024-06-12 12:12:21 -0400

    Marcando inputs do tipo radio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -75,6 +75,17 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#product').select(1).should('have.value', 'blog')
     })
 
+    it('marca o tipo de atendimento "Feedback', function() {
+        cy.get('input[type="radio"][value="feedback"]').check().should('have.value', 'feedback')
+    })
+
+    it('marca cada tipo de atendimento', function() {
+        cy.get('input[type="radio"').should('have.length', 3).each(function($radio) {
+            cy.wrap($radio).check()
+            cy.wrap($radio).should('be.checked')
+        })
+    })
+
 
 })
   
