3bd1fc14d40b81c79167bde5368ed8f81dbfd89d
menezesja <santmjad@gmail.com>
2024-06-19 08:31:11 -0400

    Simulando o viewport de um dispositivo móvel e adição do teste privacy

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -127,12 +127,5 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.contains('Talking About Testing').should('be.visible')
     })
 
-    it('testa a página da política de privacidade de forma independente', function() {
-        cy.visit('./src/privacy.html')
-
-        cy.contains('Talking About Testing').should('be.visible')
-    })
-
-
 })
   
